39fea5450419d37ead988723e7d84a0b3e1a63d9
Cupid-0x80h <ccuppid09@gmail.com>
2025-05-09 08:54:49 +0545

    Add files via upload

---
+++ fortran/io.f90
@@ -0,0 +1,6 @@
+program io
+        real :: x,y,z
+        print *, 'Enter the 3 different values x ,y ,z '
+        read *, x, y, z
+        print *, ' the value you typed for z, y, x are :' ,z ,y, x
+end program io
---
+++ fortran/sum.f90
@@ -0,0 +1,9 @@
+program sum
+        real :: answer,x,y
+        print *, 'Enter the value of Two number'
+        read *, x
+        read *, y
+        answer = x+y
+        print *, 'The total sum is ', answer
+end program sum
+
